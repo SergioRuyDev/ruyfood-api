d3e99da09c2b09329cdb21f60bd8cd0d9178e3d1
SergioRuyDev <sergioruyenator@gmail.com>
2021-04-21 17:37:32 +0400

    Initial commit

---
+++ src/main/java/com/sergioruy/ruyfoodapi/RuyfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.sergioruy.ruyfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuyfoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuyfoodApiApplication.class, args);
+    }
+
+}
