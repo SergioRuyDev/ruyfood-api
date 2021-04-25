879441e13c5869da946810be28116bc5e4d97b9c
SergioRuyDev <sergioruyenator@gmail.com>
2021-04-25 16:27:55 +0400

    Class config are created to serve beans for others classes.

---
+++ src/main/java/com/sergioruy/ruyfoodapi/RuyConfig.java
@@ -0,0 +1,17 @@
+package com.sergioruy.ruyfoodapi;
+
+import com.sergioruy.ruyfoodapi.notificacao.NotificadorEmail;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RuyConfig {
+
+    @Bean
+    public NotificadorEmail notificadorEmail() {
+        NotificadorEmail notificador = new NotificadorEmail("smtp.ruymail.com.br");
+        notificador.setCaixaAlta(true);
+
+        return notificador;
+    }
+}
