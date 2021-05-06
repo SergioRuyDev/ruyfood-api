9be84f2400a803cc4006906354e3dbca2d67a14d
SergioRuyDev <sergioruyenator@gmail.com>
2021-05-06 18:19:48 +0400

    Autowired annotation explained.

--- src/main/java/com/sergioruy/ruyfoodapi/service/AtivacaoClienteService.java
+++ src/main/java/com/sergioruy/ruyfoodapi/service/AtivacaoClienteService.java
@@ -2,19 +2,21 @@ package com.sergioruy.ruyfoodapi.service;
 
 import com.sergioruy.ruyfoodapi.modelo.Cliente;
 import com.sergioruy.ruyfoodapi.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
+    @Autowired(required = false)
     private Notificador notificador;
-
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-
-    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        if (notificador != null) {
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        } else {
+            System.out.printf("Não existe notificador, mais cliente foi ativado.");
+        }
     }
 }
